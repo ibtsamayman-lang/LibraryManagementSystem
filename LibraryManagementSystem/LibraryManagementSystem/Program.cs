23fe58f931f0f6b70982a61c03445fb80ea9e717
Ibtsam <ibtsamayman2006@gmail.com>
2026-08-20 06:33:23 +0300

    Update Layout and Read Edit

--- LibraryManagementSystem/LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Program.cs
@@ -11,11 +11,17 @@ builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+//builder.Services.AddSession(options =>
+//{
+//    options.IdleTimeout = TimeSpan.FromMinutes(30); // مدة الجلسة
+//    options.Cookie.HttpOnly = true;
+//    options.Cookie.IsEssential = true;
+//});
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // مدة الجلسة
+    options.IdleTimeout = TimeSpan.FromDays(7); // تظل الجلسة صالحة لمدة 7 أيام
     options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
+    options.Cookie.IsEssential = true; // مهم جداً: يتجاوز فحص موافقة الكوكيز
 });
 
 var app = builder.Build();
